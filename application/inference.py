05167d42680d4d612f4910ebf86f69da97fe02d1
raymund07 <raymundnilo1115@gmail.com>
2021-07-02 00:37:00 +0800

    fix

--- application/inference.py
+++ application/inference.py
@@ -18,7 +18,7 @@ from  tensorflow.keras.models import load_model
 inference_path=os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'inferencegraphs'))
 
 predictregistration_v1=tf.saved_model.load('{}/character/v1/saved_model'.format(inference_path))
-# #predictregistration_v2=tf.saved_model.load('{}/character/v2/saved_model'.format(inference_path))
+#predictregistration_v2=tf.saved_model.load('{}/character/v2/saved_model'.format(inference_path))
 # #predictregistration_v3=tf.saved_model.load('{}/character/v3/saved_model'.format(inference_path))
 predictplate=tf.saved_model.load('{}/plate/saved_model'.format(inference_path))
 jurisdiction_model = load_model('{}/jurisdiction/v1'.format(inference_path))
@@ -120,7 +120,7 @@ class Inference:
         # elif (version=='v3'):
         #     detections = predictregistration_v3(input_tensor)
         # else:
-        detections = predictregistration_v2(input_tensor)
+        detections = predictregistration_v1(input_tensor)
 
 
         num_detections = int(detections.pop('num_detections'))
